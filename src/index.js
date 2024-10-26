626d6548cce94597cb3aa76873834e41830439f8
Shakhrii <sh.ramaldanova@gmail.com>
2024-10-26 21:33:31 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let numStr = n.toString();
+  let resultStr = '';
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    resultStr += numStr[i];
+  }
+  let result = parseInt(resultStr, 10);
+  return result;
 }
